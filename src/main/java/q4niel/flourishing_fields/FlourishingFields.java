b236a3da21bf37291eac87cd1b6c161d12d2a799
q4niel <github.obscure938@passmail.net>
2024-11-26 13:31:28 +0100

    Fabric Template Mod

---
+++ src/main/java/q4niel/flourishing_fields/FlourishingFields.java
@@ -0,0 +1,14 @@
+package q4niel.flourishing_fields;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlourishingFields implements ModInitializer {
+	public static final String MOD_ID = "flourishing_fields";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/q4niel/flourishing_fields/FlourishingFieldsDataGenerator.java
@@ -0,0 +1,9 @@
+package q4niel.flourishing_fields;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlourishingFieldsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
