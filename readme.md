0f06e3a311892c994a5149741e399aff8526843b
q4niel <github.obscure938@passmail.net>
2024-12-04 15:40:07 +0100

    readme & license updated, 'gradle.properties' updated for version '0.2.0-1.21-Fabric', reworked bee flower spreading

--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 # Flourishing Fields
-
-## Features
-- **Bee Flower Spread:** Bees will now spread flowers as they pollinate, enriching the environment with more blooms.
+A Minecraft mod that adds a unique mechanic to your world.
+Bees now have a chance to spread flowers while pollinating.
+When a bee collects nectar from a flower,
+there’s a small chance that the same type of flower will spawn on a block the bee flies over while returning to its beehive.
 
 ## Feedback
 Found a bug or got some suggestions, head over to [GitHub Issues](https://github.com/q4niel/Flourishing-Fields/issues).
--- src/main/java/q4niel/flourishing_fields/mixin/BeeEntityMixin.java
+++ src/main/java/q4niel/flourishing_fields/mixin/BeeEntityMixin.java
@@ -1,8 +1,8 @@
 package q4niel.flourishing_fields.mixin;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,9 +10,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import q4niel.flourishing_fields.FlourishingFields;
 
+import java.util.Random;
+
 @Mixin(BeeEntity.class)
 public class BeeEntityMixin {
-    boolean spread = false;
+    BlockState flowerState = null;
+    boolean hasSpread = false;
 
     @Inject (
             method = "tick()V",
@@ -21,31 +24,27 @@ public class BeeEntityMixin {
     public void tick(CallbackInfo info) {
         FlourishingFields.SERVER.execute(() -> {
             BeeEntity self = (BeeEntity)(Object)this;
+            World world = FlourishingFields.SERVER.getWorld(World.OVERWORLD);
+
+            if (self.hasFlower()) {
+                flowerState = world.getBlockState(self.getFlowerPos());
+            }
+
             if (!self.hasNectar()) {
-                spread = false;
+                hasSpread = false;
                 return;
             }
 
-            if (spread || self.getFlowerPos() == null) return;
-            spread = true;
-            World world = FlourishingFields.SERVER.getWorld(World.OVERWORLD);
-
-            BlockPos srcFlowerPos = self.getFlowerPos();
-            BlockPos dstFlowerPos = switch ((int)(Math.random() * 4)) {
-                case 0 -> srcFlowerPos.north();
-                case 1 -> srcFlowerPos.south();
-                case 2 -> srcFlowerPos.west();
-                case 3 -> srcFlowerPos.east();
-                default -> null;
-            };
-            if (!world.getBlockState(dstFlowerPos).isAir()) return;
-
-            BlockState srcSoilState = world.getBlockState(srcFlowerPos.down());
-            BlockState dstSoilState = world.getBlockState(dstFlowerPos.down());
-            if (srcSoilState.equals(dstSoilState)) {
+            if (flowerState != null
+            &&  !hasSpread
+            &&  world.getBlockState(self.getBlockPos()).isAir()
+            &&  world.getBlockState(self.getBlockPos().down()).isOf(Blocks.GRASS_BLOCK)
+            &&  new Random().nextInt(100) < 5
+            ) {
+                hasSpread = true;
                 world.setBlockState (
-                        dstFlowerPos,
-                        world.getBlockState(srcFlowerPos),
+                        self.getBlockPos(),
+                        flowerState,
                         3
                 );
             }
