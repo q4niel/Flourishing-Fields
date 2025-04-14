5a551cc8433ce8da8ccd449c69e504ccf00631a7
q4niel <github.obscure938@passmail.net>
2025-04-14 14:23:50 +0200

    server & client execution helper functions

--- src/main/kotlin/dev/q4niel/flourishing_fields/FlourishingFields.kt
+++ src/main/kotlin/dev/q4niel/flourishing_fields/FlourishingFields.kt
@@ -1,12 +1,28 @@
 package dev.q4niel.flourishing_fields
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.server.MinecraftServer
 import org.slf4j.LoggerFactory
 
-object FlourishingFields : ModInitializer {
-    private val _logger:org.slf4j.Logger = LoggerFactory.getLogger("flourishing_fields");
+object FlourishingFields: ModInitializer {
+	public fun print(value: String): Unit = _logger.info(value);
+	public fun serverExec(runnable: Runnable): Unit? = _server?.execute(runnable);
+	public fun clientExec(runnable: Runnable): Unit? = _client?.execute(runnable);
 
-	override fun onInitialize():Unit {}
+	private val _logger: org.slf4j.Logger = LoggerFactory.getLogger("flourishing_fields");
+	private var _server: MinecraftServer? = null;
+	private var _client: MinecraftClient? = null;
 
-	public fun print(value:String):Unit = _logger.info(value);
+	override fun onInitialize(): Unit {
+		ServerLifecycleEvents.SERVER_STARTED.register { server ->
+			_server = server;
+		};
+
+		ClientLifecycleEvents.CLIENT_STARTED.register { client ->
+			_client = client;
+		};
+	}
 }
--- src/main/kotlin/dev/q4niel/flourishing_fields/FlourishingFieldsDataGenerator.kt
+++ src/main/kotlin/dev/q4niel/flourishing_fields/FlourishingFieldsDataGenerator.kt
@@ -3,6 +3,6 @@ package dev.q4niel.flourishing_fields
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 
-object FlourishingFieldsDataGenerator : DataGeneratorEntrypoint {
+object FlourishingFieldsDataGenerator: DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {}
 }
