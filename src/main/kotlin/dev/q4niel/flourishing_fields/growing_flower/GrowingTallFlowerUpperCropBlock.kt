135cb3189e5cf9e274a50b9a4d8b1cd5b1391919
q4niel <github.obscure938@passmail.net>
2025-04-23 15:59:47 +0200

    tall flower upper crop now breaks lower crop

--- src/main/kotlin/dev/q4niel/flourishing_fields/growing_flower/GrowingTallFlowerUpperCropBlock.kt
+++ src/main/kotlin/dev/q4niel/flourishing_fields/growing_flower/GrowingTallFlowerUpperCropBlock.kt
@@ -1,6 +1,17 @@
 package dev.q4niel.flourishing_fields.growing_flower
 
+import dev.q4niel.flourishing_fields.FlourishingFields
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.WorldAccess
 
 abstract class GrowingTallFlowerUpperCropBlock(settings: Settings?) : Block(settings) {
+    override fun onBroken(world: WorldAccess?, pos: BlockPos?, state: BlockState?) {
+        super.onBroken(world, pos, state)
+
+        FlourishingFields.serverExec {
+            world?.breakBlock(pos?.down(), false);
+        };
+    }
 }
