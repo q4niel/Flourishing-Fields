c67e8319faefb96e4e1b4ffdecd40e5dcc274928
q4niel <github.obscure938@passmail.net>
2025-04-24 13:56:48 +0200

    post datagen now replaces all _stage0s with 'flower sprout' texture

--- src/main/python/post_datagen.py
+++ src/main/python/post_datagen.py
@@ -1,28 +1,30 @@
 import os
 
-def main() -> None:
+def modelAsCross(content:str) -> str:
+    content = content.replace("crop", "cross")
+    content = content.replace("cube_all", "cross")
+    content = content.replace("all", "cross")
+    return content
+
+def stage0AsSprout(content:str, filename:str) -> str:
+    filename = filename[:-5]
+    if not "_stage0" in filename: return content
+    return content.replace(filename, "flower_sprout")
+
+if __name__ == "__main__":
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-    flowerBlockModel()
-
-    return
-
-def flowerBlockModel() -> None:
     dir = "generated/assets/flourishing_fields/models/block"
     for filename in os.listdir(dir):
-        path = os.path.join(dir, filename)
-        if not os.path.isfile(path): continue
+        fullPath = os.path.join(dir, filename)
+        if not os.path.isfile(fullPath): continue
 
-        with open(path, "r+") as file:
+        with open(fullPath, "r+") as file:
             content = file.read()
             file.seek(0)
 
-            content = content.replace("crop", "cross")
-            content = content.replace("cube_all", "cross")
-            content = content.replace("all", "cross")
+            content = modelAsCross(content)
+            content = stage0AsSprout(content, filename)
 
             file.write(content)
             file.truncate()
-    return
-
-if __name__ == "__main__": main()
