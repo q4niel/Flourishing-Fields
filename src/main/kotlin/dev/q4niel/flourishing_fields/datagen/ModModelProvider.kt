b8daf5c7b021d9f90709084a1be8302007645b37
q4niel <github.obscure938@passmail.net>
2025-04-29 13:16:50 +0200

    removed blockstate datagen due to custom implementation through 'post_datagen.py'

--- src/main/kotlin/dev/q4niel/flourishing_fields/datagen/ModModelProvider.kt
+++ src/main/kotlin/dev/q4niel/flourishing_fields/datagen/ModModelProvider.kt
@@ -1,41 +1,14 @@
 package dev.q4niel.flourishing_fields.datagen
 
-import dev.q4niel.flourishing_fields.growing_flower.GrowingFlowerCropBlock
-import dev.q4niel.flourishing_fields.growing_flower.GrowingTallFlowerLowerCropBlock
-import dev.q4niel.flourishing_fields.growing_flower.crops.GrowingFlowerCrops
 import dev.q4niel.flourishing_fields.growing_flower.seeds.GrowingFlowerSeeds
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
-import net.minecraft.block.Block
 import net.minecraft.client.data.BlockStateModelGenerator
 import net.minecraft.client.data.ItemModelGenerator
 import net.minecraft.client.data.Models
 
 class ModModelProvider(output: FabricDataOutput?) : FabricModelProvider(output) {
-    override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.DANDELION);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.POPPY);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.BLUE_ORCHID);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.ALLIUM);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.AZURE_BLUET);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.RED_TULIP);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.ORANGE_TULIP);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.WHITE_TULIP);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.PINK_TULIP);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.OXEYE_DAISY);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.CORNFLOWER);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.LILY_OF_THE_VALLEY);
-        genShortFlowerCropBlockState(generator, GrowingFlowerCrops.WITHER_ROSE);
-
-        genTallFlowerLowerCropBlockState(generator, GrowingFlowerCrops.SUNFLOWER.LOWER);
-        generator.registerSimpleCubeAll(GrowingFlowerCrops.SUNFLOWER.UPPER);
-        genTallFlowerLowerCropBlockState(generator, GrowingFlowerCrops.LILAC.LOWER);
-        generator.registerSimpleCubeAll(GrowingFlowerCrops.LILAC.UPPER);
-        genTallFlowerLowerCropBlockState(generator, GrowingFlowerCrops.ROSE_BUSH.LOWER);
-        generator.registerSimpleCubeAll(GrowingFlowerCrops.ROSE_BUSH.UPPER);
-        genTallFlowerLowerCropBlockState(generator, GrowingFlowerCrops.PEONY.LOWER);
-        generator.registerSimpleCubeAll(GrowingFlowerCrops.PEONY.UPPER);
-    }
+    override fun generateBlockStateModels(generator: BlockStateModelGenerator) {}
 
     override fun generateItemModels(generator: ItemModelGenerator) {
         generator.register(GrowingFlowerSeeds.DANDELION, Models.GENERATED);
@@ -56,21 +29,5 @@ class ModModelProvider(output: FabricDataOutput?) : FabricModelProvider(output) 
         generator.register(GrowingFlowerSeeds.LILAC, Models.GENERATED);
         generator.register(GrowingFlowerSeeds.ROSE_BUSH, Models.GENERATED);
         generator.register(GrowingFlowerSeeds.PEONY, Models.GENERATED);
-    }
-
-    private fun genShortFlowerCropBlockState(generator: BlockStateModelGenerator, cropBlock: Block) {
-        generator.registerCrop (
-            (cropBlock as GrowingFlowerCropBlock),
-            GrowingFlowerCropBlock.AGE,
-            0, 1
-        );
-    }
-
-    private fun genTallFlowerLowerCropBlockState(generator: BlockStateModelGenerator, cropBlock: Block) {
-        generator.registerCrop (
-            (cropBlock as GrowingTallFlowerLowerCropBlock),
-            GrowingTallFlowerLowerCropBlock.AGE,
-            0, 1, 2
-        );
     }
 }
--- src/main/python/post_datagen.py
+++ src/main/python/post_datagen.py
@@ -1,69 +1,98 @@
 import os
-from typing import List, Callable
+import json
+from typing import List
 
-def getDirectory() -> str:
-    return "generated/assets/flourishing_fields/models/block"
+def modID() -> str: return "flourishing_fields"
+def vanillaID() -> str: return "minecraft"
 
-def readFile(file: str) -> str:
-    with open(os.path.join(getDirectory(), file)) as f:
-        return f.read()
+def blockOfMod() -> str: return modID() + ":block/"
+def blockOfVanilla() -> str: return vanillaID() + ":block/"
 
-def writeFile(file: str, data: str) -> None:
-    with open(os.path.join(getDirectory(), file), "r+") as file:
-        file.write(data)
-        file.truncate()
+def blockstatesDir() -> str: return "generated/assets/" + modID() + "/blockstates"
+def blockModelsDir() -> str: return "generated/assets/" + modID() + "/models/block"
+
+def dumpFlowerBlockstate(flower: str) -> None:
+    with open(blockstatesDir() + "/" + flower + ".json", "w") as file:
+        json.dump({
+            "variants": {
+                "age=0": {"model": blockOfMod() + "flower_sprout"},
+                "age=1": {"model": blockOfVanilla() + flower}
+            }
+        }, file, indent=2)
     return
 
-def replaceInFile(file: str, old: str, new: str) -> None:
-    writeFile(file, readFile(file).replace(old, new))
+def dumpTallFlowerBottomBlockstate(flower: str) -> None:
+    with open(blockstatesDir() + "/" + flower + "_lower.json", "w") as file:
+        json.dump({
+            "variants": {
+                "age=0": {"model": blockOfMod() + "flower_sprout"},
+                "age=1": {"model": blockOfMod() + flower + "_bottom_middle"},
+                "age=2": {"model": blockOfVanilla() + flower + "_bottom"}
+            }
+        }, file, indent=2)
+    return
 
-def ofVanilla(name: str) -> str:
-    for flower in [
-        "dandelion",
-        "poppy",
-        "blue_orchid",
-        "allium",
-        "azure_bluet",
-        "red_tulip",
-        "orange_tulip",
-        "white_tulip",
-        "pink_tulip",
-        "oxeye_daisy",
-        "cornflower",
-        "lily_of_the_valley",
-        "wither_rose",
-        "sunflower",
-        "lilac",
-        "rose_bush",
-        "peony"
-    ]:
-        if flower in name:
-            return "minecraft:block/" + flower
-    return ""
+def dumpTallFlowerTopBlockstate(flower: str) -> None:
+    with open(blockstatesDir() + "/" + flower + "_upper.json", "w") as file:
+        json.dump({
+            "variants": {
+                "": {"model": blockOfVanilla() + flower + "_top"}
+            }
+        }, file, indent=2)
+    return
+
+def dumpFlowerModel(flower: str) -> None:
+    with open(blockModelsDir() + "/" + flower + ".json", "w") as file:
+        json.dump({
+            "parent": vanillaID() + ":block/cross",
+            "textures": {
+                "cross": modID() + ":block/" + flower
+            }
+        }, file, indent=2)
+    return
+
+def shortFlowers() -> List[str]: return [
+    "dandelion",
+    "poppy",
+    "blue_orchid",
+    "allium",
+    "azure_bluet",
+    "red_tulip",
+    "orange_tulip",
+    "white_tulip",
+    "pink_tulip",
+    "oxeye_daisy",
+    "cornflower",
+    "lily_of_the_valley",
+    "wither_rose"
+]
+
+def tallFlowers() -> List[str]: return [
+    "sunflower",
+    "lilac",
+    "rose_bush",
+    "peony"
+]
 
 def main() -> None:
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-    modId: str = "flourishing_fields:block/"
-    tallMiddleStages: List[str] = []
 
-    for name in os.listdir(getDirectory()):
-        replaceInFile(name, "crop\"", "cross\"")
-        replaceInFile(name, "cube_all\"", "cross\"")
-        replaceInFile(name, "all\"", "cross\"")
+    if not os.path.exists(blockstatesDir()):
+        os.makedirs(blockstatesDir())
 
-        if "_stage0" in name:
-            replaceInFile(name, name[:-5], "flower_sprout")
+    if not os.path.exists(blockModelsDir()):
+        os.makedirs(blockModelsDir())
 
-        if "_upper" in name:
-            replaceInFile(name, modId + name[:-5], ofVanilla(name) + "_top")
+    dumpFlowerModel("flower_sprout")
 
-        if "_stage2" in name:
-            replaceInFile(name, modId + name[:-5], ofVanilla(name) + "_bottom")
-            tallMiddleStages.append(name.replace("_stage2", "_stage1"))
+    for f in shortFlowers():
+        dumpFlowerBlockstate(f)
 
-    for name in os.listdir(getDirectory()):
-        if not "_stage1" in name or name in tallMiddleStages: continue
-        replaceInFile(name, modId + name[:-5], ofVanilla(name))
+    for f in tallFlowers():
+        dumpTallFlowerBottomBlockstate(f)
+        dumpFlowerModel(f + "_bottom_middle")
+        dumpTallFlowerTopBlockstate(f)
 
     return
+
 if __name__ == "__main__": main()
